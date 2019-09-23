f6d7b63364b546babf8ce79d66ee9ef50eae63ff
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-09-23 19:11:35 +0100

    add notificaion driver python

--- scripts/notification.py
+++ scripts/notification.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import telegram
 import datetime
 from settings import get_env_value 
@@ -8,5 +9,7 @@ def notify(photo, caption=""):
         bot = telegram.Bot(token=get_env_value("TELEGRAM_TOKEN"))
         caption = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         bot.send_photo(get_env_value("TELEGRAM_CHAT_ID"), photo=open(photo, 'rb'), caption=caption)
+    if get_env_value("NOTIFICATION_DRIVER") == 'python':
+        cv2.imshow('image', cv2.imread(photo))
     else:
         print('object detected at: ' + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
