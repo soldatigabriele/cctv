71ccdd4f5efb382c58f7358ad2d944c725c05a64
Gabriele Soldati <soldati.gabriele@gmail.com>
2020-03-30 16:03:20 +0100

    remove black frames from camera 3

--- scripts/video.py
+++ scripts/video.py
@@ -3,7 +3,7 @@ import os
 import cv2
 import numpy as np
 import datetime
-from settings import * 
+from settings import *
 import os.path as path
 from notification import notify
 
@@ -21,7 +21,7 @@ def extract_frames(video_path, output_path, frames_interval=24, video_frames_lim
         rval, frame = vc.read()
         # Here we set the frames to take (24 means 1 frame every 24, so 1 per second in our stream)
         if (c % (frames_interval) == 0) and c < int(video_frames_limit):
-            cv2.imwrite(output_path +'/'+ str(name) + '.jpg',frame)
+            cv2.imwrite(output_path + '/' + str(name) + '.jpg', frame)
             cv2.waitKey(1)
             name = name + 1
         # If we reach the max number of frames we want to extract, break the while
@@ -31,10 +31,13 @@ def extract_frames(video_path, output_path, frames_interval=24, video_frames_lim
     vc.release()
     return c
 
+
 def prepare_video(video, output_folder, event_id):
     datetime_object = datetime.datetime.now()
-    log("new video found: " + video + " at " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "info")
-    video_folder = output_folder + str(datetime_object.month) + str(datetime_object.day) + str(datetime_object.hour) + str(datetime_object.minute) + str(datetime_object.second) + str(datetime_object.microsecond)
+    log("new video found: " + video + " at " +
+        datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "info")
+    video_folder = output_folder + str(datetime_object.month) + str(datetime_object.day) + str(
+        datetime_object.hour) + str(datetime_object.minute) + str(datetime_object.second) + str(datetime_object.microsecond)
     os.mkdir(video_folder)
 
     frames_folder = video_folder + "/frames"
@@ -52,6 +55,13 @@ def prepare_video(video, output_folder, event_id):
     frames = get_list_of_files(frames_folder)
     frames.sort(key=lambda f: os.path.getmtime(f))
 
+    # Remove black frames frow camera 03
+    if camera_number == "03":
+        for frame in frames:
+            img = cv2.imread(frame)
+            if sum(img.mean(axis=0).mean(axis=0)) < 5:
+                os.remove(frame)
+
     if camera_number == "02":
         resized_frames_folder = video_folder + "/resized_frames"
         os.mkdir(resized_frames_folder)
@@ -64,7 +74,8 @@ def prepare_video(video, output_folder, event_id):
                 width = img.shape[1]
                 crop_img = img[100:height, 0:width-400]
                 # Mask the top right corner
-                trangle = np.array( [(width-1080, 0), (width-400, 0), (width-400, 340)])
+                trangle = np.array(
+                    [(width-1080, 0), (width-400, 0), (width-400, 340)])
                 cv2.drawContours(crop_img, [trangle], 0, (81, 81, 81), -1)
                 # Save the cropped image
                 cv2.imwrite(resized_frames_folder + '/' + frame_name, crop_img)
